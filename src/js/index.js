3f2e2fc7964ee5bc80a8ccc46413d0b77d2ca019
Margaret <match14@yandex.ru>
2021-07-12 23:38:14 +0300

    part page

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,48 @@
-import {$} from './common';
+import {$, Inputmask} from './common';
 
-$(window).scroll(function(){
+// move-up
+$(window).on('scroll', function(){
 	if($(this).scrollTop()>300){
 		$('.js-move-up').addClass('visible');
 	}else{
 		$('.js-move-up').removeClass('visible');
 	}
 });
-$('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
+$('.js-move-up').on('click', function(){$('body,html').animate({scrollTop:0},800);return false;});
+
+// Маска для телефона
+Inputmask('+7 (999) 999-9999').mask('.js-phone');
+
+// catalog slider
+if($('.js-cat-list-slider').length){
+	$('.js-cat-list-slider').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		prevArrow: '<button id="prev" type="button" class="btn-arr btn-arr_left"><svg class="icon ic-arrow-left" width="16" height="30"><use xlink:href="assets/sprites/sprite.svg#ic-arrow-left"></use></svg></button>',
+		nextArrow: '<button id="next" type="button" class="btn-arr btn-arr_right"><svg class="icon ic-arrow-right" width="16" height="30"><use xlink:href="assets/sprites/sprite.svg#ic-arrow-right"></use></svg></button>',
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			},
+		]
+	});
+}
